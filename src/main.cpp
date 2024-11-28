883c0e770e46a547a07cd47a743423753faef051
Anton Sidorov <sidorov@opsira.de>
2024-11-28 09:52:20 +0100

    ColorMap probes

--- src/main.cpp
+++ src/main.cpp
@@ -57,6 +57,19 @@ namespace
         }
     };
 
+    class ColorMap1 : public QwtLinearColorMap
+    {
+    public:
+        ColorMap1(int n = 18) :
+                QwtLinearColorMap(Qt::color0,QColor(static_cast<Qt::GlobalColor>(n)))
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                addColorStop((double)i/(double)n, QColor(static_cast<Qt::GlobalColor>(i)));
+            }
+        }
+    };
+
     class ColorMap : public QwtLinearColorMap
     {
     public:
@@ -70,12 +83,25 @@ namespace
     class ColorMapLog : public QwtLinearColorMap
     {
     public:
-        ColorMapLog() :
+        ColorMapLog(int n = 10) :
                 QwtLinearColorMap(Qt::blue, Qt::red)
         {
-            addColorStop(0.001, Qt::blue);
-            addColorStop(0.01, Qt::green);
-            addColorStop(0.1, Qt::red);
+            auto jet = QwtLinearColorMap(Qt::blue, Qt::red);
+            jet.addColorStop(0.5, Qt::green);
+
+            // get colors
+            QList<QRgb> linCol;
+            for (auto i = 1; i < n; i++)
+            {
+                linCol << jet.rgb({0.,1.}, i/(double)n);
+            }
+
+            // shift color positions
+            auto q = pow(1000., 1. / (double)n);
+            for (auto i = 1; i < n; i++)
+            {
+                addColorStop(pow(q, (double)i) / 1000., linCol[i-1]);
+            }
         }
     };
 
@@ -116,6 +142,10 @@ namespace
             auto pos = canvas()->mapFromGlobal(QCursor::pos());
             auto width = canvas()->geometry().width();
             auto height = canvas()->geometry().height();
+            if(polygon.size()<2)
+            {
+                return polygon;
+            }
             if ((pos.x() < 0.05 * width || 0.95 * width < pos.x())
                 && 0.05 * height < pos.y() && pos.y() < 0.95 * height) {
                 QVector<QPoint> points;
@@ -186,19 +216,39 @@ namespace
     public:
         QwtInterval interval(Qt::Axis axis) const override
         {
-            qDebug() << value(1.,1.);
-            qDebug() << value(0.,0.);
             if(axis == Qt::ZAxis){
                 return {value(1.,1.), value(0.,0.)};
             }
-
             return {-1,1};
         }
 
         double value(double x, double y) const override
         {
-//            return 1000.*exp(-x*x/0.03)*exp(-y*y/0.001) + 0.1;
-            return log(1000*exp(-x*x/0.03)*exp(-y*y/0.001) + 0.1);
+            return 999.9*exp(-x*x/0.03)*exp(-y*y/0.001) + 0.1;
+        }
+    };
+
+    class TestDataLog: public QwtRasterData
+    {
+    public:
+        QwtInterval interval(Qt::Axis axis) const override
+        {
+            if(axis == Qt::ZAxis){
+                return {value(1.,1.), value(0.,2.)};
+            }
+            else if (axis == Qt::XAxis){
+                return {-1.,1.};
+            }
+            else if (axis == Qt::YAxis){
+                return {1.,3.};
+            }
+            return {-1,1};
+        }
+
+        double value(double x, double y) const override
+        {
+            auto minVal = 999.9*exp(-1/0.03)*exp(-1/0.001) + 0.1;
+            return log((999.9*exp(-x*x/0.03)*exp(-(y-2)*(y-2)/0.001) + 0.1)/minVal);
         }
     };
 
@@ -209,8 +259,12 @@ MainWindow::MainWindow(QWidget *parent)
 {
     auto plot = new QwtPlot(this);
 
+    auto grid = new Grid();
+    grid->setZ(12);
+    grid->attach(plot);
+
     QImage image;
-    image.load("/home/sid/prog/ViewPortQwt/lena.png");
+    image.load("lena.png");
     image.convertTo(QImage::Format_ARGB32);
     image.setDotsPerMeterX(image.width());
     image.setDotsPerMeterY(image.height());
@@ -218,7 +272,7 @@ MainWindow::MainWindow(QWidget *parent)
     auto imageItem = new QwtRasterImage(image, {0.1 * image.width() / image.dotsPerMeterX(),
                                                 0.5 * image.height() / image.dotsPerMeterY()});
     imageItem->setZ(1);
-    imageItem->attach(plot);
+//    imageItem->attach(plot);
 
     auto image1 = QImage(image);
     image1.setDotsPerMeterX(5120);
@@ -227,52 +281,83 @@ MainWindow::MainWindow(QWidget *parent)
 
     imageItem1->setZ(2);
     imageItem1->setAlpha(255);
-    imageItem1->attach(plot);
+//    imageItem1->attach(plot);
 
     auto gauss = new QwtPlotSpectrogram();
     gauss->setData(new TestData());
     gauss->attach(plot);
-    gauss->setZ(3);
-    gauss->setAlpha(125);
-    gauss->setColorMap(new ColorMap());
+    // gauss->setZ(3);
+    // gauss->setAlpha(125);
+    gauss->setColorMap(new ColorMapLog());
 
-    auto grid = new Grid();
-    grid->setZ(12);
-    grid->attach(plot);
+    auto gaussLog = new QwtPlotSpectrogram();
+    gaussLog->setData(new TestDataLog());
+    gaussLog->attach(plot);
+    // gauss->setZ(3);
+    // gauss->setAlpha(125);
+    gaussLog->setColorMap(new ColorMap());
 
-    //auto  zInterval = QwtInterval{0, 255};
-    auto zInterval = gauss->interval(Qt::ZAxis);
+    // auto  zInterval = QwtInterval{0, 255};
+    // auto zInterval = gauss->interval(Qt::ZAxis);
+    // auto scale = QwtLinearScaleEngine().divideScale(zInterval.minValue(), zInterval.maxValue(), 10, 5);
+
+    // auto rightAxis = plot->axisWidget(QwtPlot::yRight);
+    // rightAxis->setColorBarEnabled(true);
+    // rightAxis->setColorMap(zInterval, new ColorMap());
+    // rightAxis->setColorBarWidth(30);
+
+    // plot->setAxisVisible(QwtPlot::yRight);
+    // plot->setAxisAutoScale(QwtPlot::yRight, false);
+    // plot->setAxisScaleDiv(QwtPlot::yRight, scale);
+    // plot->setAxisTitle(QwtPlot::yRight, "z Scale");
+
+
+     // auto zScaleWidget = new QwtScaleWidget(QwtScaleDraw::RightScale, this);
+     // zScaleWidget->setTitle("Intensity");
+     // zScaleWidget->setColorBarEnabled(true);
+    // zScaleWidget->setColorMap(zInterval, new ColorMap());
+    // zScaleWidget->setColorBarWidth(30);
+    // zScaleWidget->setScaleDiv(scale);
+
+    auto zInterval = QwtInterval{gauss->interval(Qt::ZAxis).minValue(), gauss->interval(Qt::ZAxis).maxValue()};
     auto scale = QwtLinearScaleEngine().divideScale(zInterval.minValue(), zInterval.maxValue(), 10, 5);
-
-    auto rightAxis = plot->axisWidget(QwtPlot::yRight);
-    rightAxis->setColorBarEnabled(true);
-    rightAxis->setColorMap(zInterval, new ColorMap());
-    rightAxis->setColorBarWidth(30);
-
-    plot->setAxisVisible(QwtPlot::yRight);
-    plot->setAxisAutoScale(QwtPlot::yRight, false);
-    plot->setAxisScaleDiv(QwtPlot::yRight, scale);
-    plot->setAxisTitle(QwtPlot::yRight, "z Scale");
-
-
-    auto zScaleWidget = new QwtScaleWidget(QwtScaleDraw::RightScale, this);
-    zScaleWidget->setTitle("Intensity");
-    zScaleWidget->setColorBarEnabled(true);
-    zScaleWidget->setColorMap(zInterval, new ColorMap());
-    zScaleWidget->setColorBarWidth(30);
-    zScaleWidget->setScaleDiv(scale);
+    auto scaleLog = QwtLogScaleEngine().divideScale(zInterval.minValue(), zInterval.maxValue(), 10, 10);
 
     auto zScaleWidgetLog = new QwtScaleWidget(QwtScaleDraw::RightScale, this);
-    auto zIntervalLog = QwtInterval{exp(gauss->interval(Qt::ZAxis).minValue()), exp(gauss->interval(Qt::ZAxis).maxValue())};
     zScaleWidgetLog->setTitle("Intensity Log");
     zScaleWidgetLog->setColorBarEnabled(true);
-    zScaleWidgetLog->setColorMap(zIntervalLog, new ColorMapLog());
+    zScaleWidgetLog->setColorMap(zInterval, new ColorMapLog());
     zScaleWidgetLog->setColorBarWidth(15);
-
-    scale = QwtLogScaleEngine().divideScale(zIntervalLog.minValue()  , zIntervalLog.maxValue(), 10, 10);
     zScaleWidgetLog->setScaleDiv(scale);
 
+
+    auto zScaleWidget = new QwtScaleWidget(QwtScaleDraw::RightScale);
+    zScaleWidget->setTitle("Intensity Linear");
+    zScaleWidget->setColorBarEnabled(true);
+    zScaleWidget->setColorMap(zInterval, new ColorMap());
+    zScaleWidget->setColorBarWidth(15);
+    zScaleWidget->setScaleDiv(scale);
+
+    auto zScaleWidgetLogLog = new QwtScaleWidget(QwtScaleDraw::RightScale);
+    zScaleWidgetLogLog->setTitle("Intensity Log Log");
+    zScaleWidgetLogLog->setColorBarEnabled(true);
+    zScaleWidgetLogLog->setColorMap(zInterval, new ColorMapLog());
+    zScaleWidgetLogLog->setColorBarWidth(15);
+    zScaleWidgetLogLog->setScaleDiv(scaleLog);
+
+    auto zScaleWidgetLinLog = new QwtScaleWidget(QwtScaleDraw::RightScale);
+    zScaleWidgetLinLog->setTitle("Intensity Linear Log");
+    zScaleWidgetLinLog->setColorBarEnabled(true);
+    zScaleWidgetLinLog->setColorMap(zInterval, new ColorMap());
+    zScaleWidgetLinLog->setColorBarWidth(15);
+    zScaleWidgetLinLog->setScaleDiv(scaleLog);
+
+
+    // scale = QwtLogScaleEngine().divideScale(zIntervalLog.minValue()  , zIntervalLog.maxValue(), 10, 10);
+    // zScaleWidgetLog->setScaleDiv(scale);
+
     auto *magnifier = new QwtPlotMagnifier(plot->canvas());
+    magnifier->setAxisEnabled(Qt::YAxis, false);
 
     auto *panner = new QwtPlotPanner(plot->canvas());
     panner->setAxisEnabled(QwtPlot::yRight, false);
@@ -295,8 +380,10 @@ MainWindow::MainWindow(QWidget *parent)
 
     auto gLayout = new QGridLayout();
     gLayout->addWidget(plot, 0, 0);
-    gLayout->addWidget(zScaleWidget, 0, 1);
-    gLayout->addWidget(zScaleWidgetLog, 0, 2);
+    gLayout->addWidget(zScaleWidgetLog, 0, 1);
+    gLayout->addWidget(zScaleWidget, 0, 2);
+    gLayout->addWidget(zScaleWidgetLogLog, 0, 3);
+    gLayout->addWidget(zScaleWidgetLinLog, 0, 4);
 
     QWidget *window = new QWidget();
     window->setLayout(gLayout);
